32e5efc4b6b91f9fef0022d769ffd12599bdf9c5
shaikhNoorAbbas <shaikhnoor1512@gmail.com>
2026-01-18 22:32:01 +0530

    image upload

---
+++ routes/imageRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+let router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Image Upload");
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import imageRoutes from "./routes/imageRouter.js";
+let app = express();
+
+app.use(express.json());
+
+//  routes
+app.use("/api/images", imageRoutes);
+
+let port = 8000;
+
+app.listen(port, () =>
+  console.log(`port is running on: ${port}\n -> http://localhost:${port}`)
+);

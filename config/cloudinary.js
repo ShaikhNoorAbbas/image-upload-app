a9e1e402bf54bb54a82eb82951fa2eeddf1e9523
shaikhNoorAbbas <shaikhnoor1512@gmail.com>
2026-01-28 17:48:20 +0530

    added cloudinary config file

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,1 +1,8 @@
-console.log("Cloudinary js");
+import { v2 as cloudinary } from "cloudinary";
+cloudinary.config({
+  cloud_name: "dtflwgaec",
+  api_key: "255237959211577",
+  api_secret: "ZFQUAgw6NYlYohT6iDnnQsGiNBA",
+});
+
+export default cloudinary

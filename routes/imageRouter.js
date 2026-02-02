bd6046938e900b2db59e7d1b59e815a0333b13c6
Rahul <rahul.dvoc@gmail.com>
2026-02-02 11:45:34 +0530

    added connection code

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { uploadSingleImage } from "../middlewares/multer.js";
+import cloudinary from "../config/cloudinary.js";
+import fs from "fs/promises";
 let router = express.Router();
 
 router.get("/", (req, res) => {
@@ -7,15 +9,29 @@ router.get("/", (req, res) => {
 });
 
 //
-router.post("/", uploadSingleImage, (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({
-      message: "No Image Uploaded",
-    });
-  } else {
-    return res.status(200).json({
-      message: "Image Uploaded Successfully",
-      file: req.file,
+router.post("/", uploadSingleImage, async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "No Image Uploaded",
+      });
+    } else {
+      // sending data to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "imageUpload",
+      });
+      // deleting local file
+      fs.unlink(req.file.path);
+      return res.status(200).json({
+        message: "Image Uploaded Successfully",
+        imageUrl: result.secure_url,
+        public_id: result.public_id,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Image upload failed",
     });
   }
 });
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import imageRoutes from "./routes/imageRouter.js";
+import mongoose from "mongoose";
+
 let app = express();
 
 app.use(express.json());
-
+let mongodbUri =
+  "mongodb+srv://imageupload:imageupload@cluster0.guor3sz.mongodb.net/";
 //  routes
 app.use("/api/images", imageRoutes);
+//Connection to Database
+mongoose
+  .connect(mongodbUri, {
+    dbName: "imageupload",
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log("Error While Connecting to Database", error));
 
-
-let port = 8000;
+let port = 2000;
 app.listen(port, () =>
   console.log(`port is running on: ${port}\n -> http://localhost:${port}`)
 );
